f96fe5745dbe323d579d8da6bec3286f11bab828
Link <djoao.pedro2012@gmail.com>
2024-11-04 22:06:19 -0300

    Login

---
+++ src/auth/helpers/messages.helper.ts
@@ -0,0 +1,5 @@
+export const MessagesHelper = {
+    AUTH_LOGIN_NOT_FOUND: 'Login informado não e valido',
+    AUTH_PASSWORD_NOT_FOUND: 'Senha infromada não e valida',
+    AUTH_PASSWORD_OR_LOGIN_NOT_FOUND: 'Email e/ou senha não encontrados.',
+}
